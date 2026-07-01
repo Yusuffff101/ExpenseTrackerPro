abc37a2832a7f1be668d6759c5a7c8d9ef70821e
Yusuf <yusufbootwala83@gmail.com>
2026-07-01 17:19:20 +0530

    Fixed bugs in DB

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,9 @@
+import os
 from sqlalchemy.ext.asyncio import create_async_engine
 
-DATABASE_URL = (
+# Read from the environment variable first. If it doesn't exist, use the local string.
+DATABASE_URL = os.getenv(
+    "DATABASE_URL", 
     "postgresql+asyncpg://postgres:123456@localhost:5432/expense_tracker"
 )
 
